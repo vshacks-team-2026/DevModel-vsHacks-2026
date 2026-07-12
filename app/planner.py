31464b17fcf61469bf439daa0c1fb1b462e97637
Art-eri-ya <gerasartem4@gmail.com>
2026-07-11 22:36:18 -0400

    added recipe filtering and type sprting

---
+++ app/planner.py
@@ -0,0 +1,77 @@
+
+def budget_converter(budget):
+    try:
+        budget = int(budget)
+    except (TypeError, ValueError):
+        return None
+    if budget < 100:
+        return "cheap"
+    elif budget < 200:
+        return "moderate"
+    return "expensive"
+
+def filter_by_budget(recipes, cost_range):
+    allowed_ranges = {
+        "cheap": ["cheap"],
+        "moderate": ["cheap", "moderate"],
+        "expensive": ["cheap", "moderate", "expensive"],
+    }
+    budget_recipes = []
+    for recipe in recipes:
+        if recipe.cost_range in allowed_ranges[cost_range]:
+            budget_recipes.append(recipe)
+
+    return budget_recipes
+
+def filter_by_cuisine(recipes, selected_cuisine):
+    if not selected_cuisine:
+        return recipes
+
+    cuisine_recipes = []
+    for recipe in recipes:
+        if recipe.cuisine in selected_cuisine:
+            cuisine_recipes.append(recipe)
+    return cuisine_recipes
+
+def filter_by_allergens(recipes, selected_allergens):
+    if not selected_allergens:
+        return recipes
+
+    allergens_recipes = []
+    for recipe in recipes:
+        contain_allergens = any(allergen in selected_allergens for allergen in recipe.allergens)
+        if not contain_allergens:
+            allergens_recipes.append(recipe)
+    return allergens_recipes
+
+def apply_filters(recipes, budget, selected_cuisines, selected_allergens):
+    converted_budget = budget_converter(budget)
+    if converted_budget is None:
+        return []
+    budget_recipes = filter_by_budget(recipes, converted_budget)
+    cuisine_recipes = filter_by_cuisine(budget_recipes, selected_cuisines)
+    filtered_recipes = filter_by_allergens(cuisine_recipes, selected_allergens)
+    return filtered_recipes
+
+def group_by_type(recipes):
+    grouped_recipes = {
+        "breakfast": [],
+        "lunch": [],
+        "dinner": [],
+    }
+    for recipe in recipes:
+        if recipe.type in ["breakfast", "lunch", "dinner"]:
+            grouped_recipes[recipe.type].append(recipe)
+    return grouped_recipes
+
+if __name__ == "__main__":
+    from app import app
+    from models import db, Recipe
+
+    with app.app_context():
+        recipes = Recipe.query.all()
+
+        grouped_recipes = group_by_type(recipes)
+
+        for meal_type, meal_recipes in grouped_recipes.items():
+            print(meal_type, [recipe.name for recipe in meal_recipes])
